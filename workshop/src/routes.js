b91f7c3b14c0d05f1c3eaebd9a7f38defe6de064
Daniel Delchev <kdanieldelchev1301@abv.bg>
2022-09-13 22:02:49 -0400

    login and register page are visible

--- workshop/src/routes.js
+++ workshop/src/routes.js
@@ -5,11 +5,13 @@ const aboutController = require('../controllers/aboutController');
 const detailsController = require('../controllers/detailsController');
 const createCubeController = require('../controllers/createCubeController');
 const accessoryCreateController = require('../controllers/accessoryCreateController');
+const authController = require('../controllers/authController');
 
 router.use('/create', createCubeController);
 router.use('/accessory', accessoryCreateController);
 router.use('/', homeController);
 router.use('/about', aboutController);
 router.use('/details', detailsController);
+router.use('/auth', authController);
 
 module.exports = router;
